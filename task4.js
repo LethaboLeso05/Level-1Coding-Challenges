a2d9ab5d772cd3d2af386900a739ab42fef08029
LethaboLeso05 <lethabo_leso@icloud.com>
2025-01-12 17:23:57 +0200

    task4 complete

---
+++ task4.js
@@ -0,0 +1,11 @@
+function square(num) {
+    for (let i = 0; i < num; i++) {
+      let row = '';
+      for (let j = 0; j < num; j++) {
+        row += '#';
+      }
+      console.log(row);
+    }
+  }
+
+module.exports = { square };
